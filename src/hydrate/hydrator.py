9dcbc063fc794f076a01248a5a2f604f25722150
satej soman <satej.soman@gmail.com>
2020-06-05 02:52:35 -0500

    hydration setup

---
+++ src/hydrate/hydrator.py
@@ -0,0 +1,5 @@
+from mrjob import MRJob
+
+class BaseHydrator(MRJob):
+    def __init__(self, table_name: str):
+        pass 
---
+++ src/upload/upload.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+# script to upload blocks and building footprints from Midway to S3.
+
+# check we have everything we need 
+module load python/3.7.0
+pip3 install awscli --user
+ls ~/.aws/credentials 
+aws="python -mawscli"
+
+# vars
+DATA="/project2/bettencourt/mnp/prclz/data"
+$aws s3 mb s3://realignment-feasibility-buildings
+$aws s3 mb s3://realignment-feasibility-blocks
+
+for country in "$@"; do 
+    $aws s3 cp ${DATA}"/blocks/${country}"    s3://realignment-feasibility-blocks/"${country}"    --recursive
+    $aws s3 cp ${DATA}"/buildings/${country}" s3://realignment-feasibility-buildings/"${country}" --recursive
+done 
