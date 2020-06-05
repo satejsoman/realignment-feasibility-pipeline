5978626015b0b8057de5ca5592e46ae41ee5ee5d
satej soman <satej.soman@gmail.com>
2020-06-05 09:48:53 -0500

    reorg

--- src/hydrate/hydrator.py
+++ src/hydrate/hydrator.py
@@ -1,5 +1,27 @@
-from mrjob import MRJob
+import geopandas as gpd
+import pandas as pd
+from geoalchemy2 import Geometry, WKTElement
+from mrjob.job import MRJob, MRStep
+from sqlalchemy import create_engine
+
 
 class BaseHydrator(MRJob):
-    def __init__(self, table_name: str):
-        pass 
+    def __init__(self, table_name: str, s3_prefix: str, *args, **kwargs):
+        super(BaseHydrator, self).__init__(*args, **kwargs)
+        self.table_name = table_name 
+        self.s3_prefix = s3_prefix
+        self.engine = None
+
+    def mapper_init(self):
+        self.engine = create_engine("postrgresql://postgres:blorpblorp@:5432/database-1")
+
+    def mapper_raw(self, path, uri):
+        gpd.read_file(path).to_sql(
+            self.table_name
+        )
+
+    def mapper_final(self):
+        self.engine.dispose()
+
+    def steps(self):
+        return [MRStep(mapper_init=self.mapper_init, mapper_raw=self.mapper_raw, mapper_final=self.mapper_final)]
--- src/hydrate/setup.sql
+++ src/hydrate/setup.sql
@@ -1,16 +1,12 @@
 -- sanity check 
 select current_user;
-select PostGIS_full_version();
 
 -- create extensions
 create extension postgis;
 create extension fuzzystrmatch;
-create extension postgis_tiger_geocoder;
 create extension postgis_topology;
 
 -- in-band permissions
-alter schema tiger owner to rds_superuser;
-alter schema tiger_data owner to rds_superuser;
 alter schema topology owner to rds_superuser;
 
 CREATE FUNCTION exec(text) returns text language plpgsql volatile AS $f$ BEGIN EXECUTE $1; RETURN $1; END; $f$;
@@ -19,11 +15,15 @@ SELECT exec('ALTER TABLE ' || quote_ident(s.nspname) || '.' || quote_ident(s.rel
   FROM (
     SELECT nspname, relname
     FROM pg_class c JOIN pg_namespace n ON (c.relnamespace = n.oid) 
-    WHERE nspname in ('tiger','topology') AND
+    WHERE nspname in ('topology') AND
     relkind IN ('r','S','v') ORDER BY relkind = 'S')
 s; 
 
 -- test 
-select na.address, na.streetname, na.streettypeabbrev, na.zip
-from normalize_address('1 Devonshire Place, Boston, MA 02109') as na;
 select topology.createtopology('my_new_topo',26986,0.5);
+
+-- make like a carpenter and build some tables 
+create table blocks; 
+create table buildings;
+
+-- set up geospatial indices 
